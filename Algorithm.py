b51c15dc73fc8831ec2e942ce2864d5ebfbff353
tzanidis <tzanidis@ualberta.ca>
2016-04-05 18:56:45 -0600

    bug fixer

--- Algorithm.py
+++ Algorithm.py
@@ -2,58 +2,77 @@ import Game
 
 class Winnable():
 
-	def getAccessTiles(gameMap, accessibleTiles = None):
+	def isWinnable(gameMap):
 		character = Game.Character(gameMap)
-		requirementsGotten = list()
-		if accessibleTiles == None:
-			accessibleTiles = [character.tileOn]
+		requirementsGotten = set(character.gear)
+		accessibleTiles = set()
+		searchTiles = set([character.tileOn])
+		specSearch = set()
 
-		searchTiles = accessibleTiles
-		specSearch = list()
 		while searchTiles:
 			tileTerm = searchTiles.pop()
 			#Base cases
-			if tileTerm in accessibleTiles:
+			if tileTerm in accessibleTiles: #Check if tile already checked
 				continue
-			if tileTerm.requirement == "gate":
-				#Check if requirements contain gate key or gear
+			if tileTerm.requirement == "gate": #Check if requirements contain gate key
 				if "Gate Key" not in requirementsGotten:
-					specSearch.append(tileTerm)
+					specSearch.add(tileTerm)
 					continue
-			if tileTerm.requirement == "hills":
+			if tileTerm.requirement == "hills": #Check if requirements contain gear
 				if "Climbing Gear" not in requirementsGotten:
-					specSearch.append(tileTerm)
+					specSearch.add(tileTerm)
 					continue
-			accessibleTiles.append(tileTerm)
+
+			accessibleTiles.add(tileTerm) #Tile is accessible add to list of accessible tiles (accessibleTiles)
+
 			if tileTerm.requirementLoot == "Gate Key" or tileTerm.requirementLoot == "Climbing Gear":
 				if tileTerm.requirementLoot not in requirementsGotten:
-					requirementsGotten.append(tileTerm.requirementLoot)
+					requirementsGotten.add(tileTerm.requirementLoot)
 					if tileTerm.requirementLoot == "Gate Key":
 						for term in specSearch:
 							if term.requirement == "gate":
-								searchTiles.append(term)
+								searchTiles.add(term)
 					elif tileTerm.requirementLoot == "Climbing Gear":
 						for term in specSearch:
 							if term.requirement == "hills":
-								searchTiles.append(term)
-			if tileTerm.label == "end":
-				break
-			character.TileOn = tileTerm
-			for direction in ["up","right","left","down","stay"]:
+								searchTiles.add(term)
+
+			if tileTerm.label == "end": #if tile is end, game is winnable
+				return True
+
+			character.tileOn = tileTerm
+			for direction in ["up","right","left","down"]:
 				tile = Game.Movement.getTileInDir(gameMap, character, direction)
 				if tile != None:
-					searchTiles.append(tile)
+					searchTiles.add(tile)
+		return False #checked all accessible tiles and not are end, game is not winnable
+	
+	#def minExp(gameMap):
+		'''
+		Ideas:
+		Brute force every path. O(5^n) if n = map tiles
+		Two 'AI's' (heuretics needed):
+		1) Low level and find a weapon, then path to boss (path around locations based on movement)
+		2) High level then path to boss on last turn/or until boss is defeated efficiently
+		Final) Get high attack and then path to boss. Path to boss when attack is efficiently high at killing boss.
 
 
+		'''
 
-def isWinnable(gameMap):
-	#create accesible tiles list
-	accessibleTiles = getAccessTiles(gameMap)
-	if gameMap.map[gameMap.end] is in accessibleTiles: #access includes end tile
+	#def minTurns(gameMap):
+		#lol easy
+
+
+def howWinnable(gameMap):
+	if Winnable.isWinnable(gameMap):
+		print("Game is winnable... ")
+		#Winnable.minExp(gameMap)
+		#Winnable.minTurns(gameMap)?
 		return True
+
 	else:
+		print("Game is not winnable, making new map.")
 		return False
 
-
 if __name__ == "__main__":
-	isWinnable(Game.Map(False))
+	howWinnable(Game.Map(False))
